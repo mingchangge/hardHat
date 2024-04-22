f177d89e7ceea4309fdc42897a3ed2c3c69526c9
lxq <lxq@webterren.com>
2024-04-22 15:13:22 +0800

    second commit

--- contracts/SimplePaymentChannel.sol
+++ contracts/SimplePaymentChannel.sol
@@ -17,7 +17,7 @@ contract SimplePaymentChannel {
   }
 
   // 关闭通道并将余额发送给接收者
-  function close(uint256 amount, bytes memory signature) external {
+  function close(uint256 amount, bytes memory signature) external view {
     require(msg.sender == recipient, 'Recipient only');
     require(isValidSignature(amount, signature), 'Invalid signature');
   }
@@ -29,13 +29,25 @@ contract SimplePaymentChannel {
     return recoverSigner(message, signature) == sender;
   }
 
-  function recoverSigner(
-    bytes32 message,
-    bytes memory sig
-  ) internal pure returns (uint8 v, bytes32 r, bytes32 s) {}
-
   /// 构建一个前缀哈希值以模仿以太坊的签名行为
   function prefixed(bytes32 hash) internal pure returns (bytes32) {
     return keccak256(abi.encodePacked('\x19Ethereum Signed Message:\n32', hash));
+  }
+
+  function recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address) {
+    (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+    return ecrecover(message, v, r, s);
+  }
+
+  /// 创建和验证签名
+  function splitSignature(bytes memory sig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+    require(sig.length == 65, 'Invalid signature length');
+
+    assembly {
+      r := mload(add(sig, 32))
+      s := mload(add(sig, 64))
+      v := byte(0, mload(add(sig, 96)))
+    }
+    return (v, r, s);
   }
 }
